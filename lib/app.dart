33d2a9446f047ec13c7383a17187034b71729087
Sidnei Polo <sidnei.polo@arctouch.com>
2019-06-01 12:17:28 -0300

    :lipstick: Home page update

---
+++ lib/app.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hack_19/module/register_bloc.dart';
+import 'package:hack_19/module/register_events.dart';
+import 'package:hack_19/view/home_view.dart';
+import 'package:hack_19/view/resources/theme_colors.dart';
+
+class App extends StatelessWidget {
+
+  final RegisterBloc registerBloc;
+
+  App(this.registerBloc) {
+    registerBloc.dispatch(Fetch());
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProviderTree(
+        blocProviders: [
+          BlocProvider<RegisterBloc>(bloc: registerBloc),
+        ],
+        child: new MaterialApp(
+          theme: new ThemeData(
+              primaryColor: ThemeColors.accent,
+              accentColor: ThemeColors.accent,
+              backgroundColor: ThemeColors.backgroundColor,
+              scaffoldBackgroundColor: ThemeColors.scaffoldBackgroundColor,
+              cardColor: ThemeColors.white,
+              bottomAppBarTheme: BottomAppBarTheme(color: ThemeColors.white, elevation: 0.0),
+              iconTheme: IconThemeData(color: ThemeColors.white),
+              primaryTextTheme: TextTheme(title: TextStyle(color: ThemeColors.white)),
+          ),
+          home: new HomeView(),
+        )
+    );
+  }
+}
---
+++ lib/view/resources/theme_colors.dart
@@ -0,0 +1,16 @@
+import 'dart:ui';
+
+abstract class ThemeColors {
+
+  static const accent = Color(0xFF43CBC8);
+
+  static const white = Color(0xFFFFFFFF);
+
+  static const grey = Color(0xFF949494);
+
+  static const backgroundColor = Color(0xFFEEF3FC);
+
+  static const scaffoldBackgroundColor = Color(0xFFF8F8F8);
+
+  static const shadowColor = Color(0x16000000);
+}
