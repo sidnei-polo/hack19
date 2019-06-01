48095833538294db6e78d88d17f8ba31bbf4b7b5
William César Rodrigues <william.rodrigues@live.fae.edu>
2019-06-01 14:42:27 -0300

    Lista alterada

--- lib/data/register_repository.dart
+++ lib/data/register_repository.dart
@@ -4,7 +4,9 @@ class RegisterRepository {
 
   Future<List<Register>> fetch(int page) async {
     var mockEvents = [
-      Register(title: "Title", description: "Problema na rodovia", dateTime: DateTime.now(), status: Status.open),
+      Register(title: "Burraco na rua XV de Novembro", description: "Burraco na rua XV de Novembro, em frete a Universidade Positivo", dateTime: DateTime.now().add(new Duration(days: -45)), status: Status.open),
+      Register(title: "Calçado da Pc. Rui Barbosa com defeito", description: "Calçado da Pc. Rui Barbosa com defeito, pedras soltas...", dateTime: DateTime.now().add(new Duration(days: -30)), status: Status.open),
+      Register(title: "Furtos no Pq. Nautico", description: "Furto no Pq. Nautico, Samsung 7 Edge, mochila e documentos...", dateTime: DateTime.now(), status: Status.open)
     ];
     return mockEvents;
   }
